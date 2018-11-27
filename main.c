ec64509964f5bc9b17a22e021bfd1afdb24304e2
NehaMethiwala <33860953+NehaMethiwala@users.noreply.github.com>
2018-11-27 14:25:31 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,11 @@
 #define max 5
 main()
 {
-arr[max]={1,3,4,5,8},i;
+arr[max]={1,3,4,5,8},i,sum=0;
 for(i=0;i<max;i++)
 {
 printf("%d \n",arr[i]);
+  sum+=arr[i];
 }
+  printf("sum --- %d\n",sum);
 }
